4bff5ffa72392851342ef5c6c89613af70eba716
Pranay Jumde <pranayjumde13@gmail.com>
2023-11-22 16:04:53 +0530

    Initial commit

---
+++ office-desk/src/main/java/com/coderview/officedesk/OfficeDeskApplication.java
@@ -0,0 +1,13 @@
+package com.coderview.officedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeDeskApplication.class, args);
+	}
+
+}
---
+++ office-desk/src/test/java/com/coderview/officedesk/OfficeDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderview.officedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
